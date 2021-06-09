907968aba5ce264366a4c267abfe60f7266ccd58
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-09 17:03:42 +0400

    formated some fields to int and float

--- soup.py
+++ soup.py
@@ -14,11 +14,20 @@ def get_data():
     data = []
     for i, tr in enumerate(tr_list):
         if 1 < i < 260:
-            data.append([ elem for i, elem in enumerate(tr.text.split('\n')) if i not in black_list_of_indexes and elem and elem!= '+' and '\t' not in elem])
-    # for i, td in enumerate(data):
-    #     for j, elem in enumerate(td):
-    #         if j == 7:
-    #             data[i][j] = float(elem[:-1])
+            data.append([elem for i, elem in enumerate(tr.text.split('\n')) if
+                         i not in black_list_of_indexes and elem and elem != '+' and '\t' not in elem])
+    for i, td in enumerate(data):
+        for j, elem in enumerate(td):
+            to_float_list = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+            if j in to_float_list:
+                try:
+
+                    data[i][j] = float(elem[:-1].replace(' ', ''))
+                except:
+
+                    data[i][j] = int(elem)
+
     return data
 
-pprint(get_data())
+
+# pprint(get_data())
