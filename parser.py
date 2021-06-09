06f66532a1bef476e3b9701b155f9c394b12fa43
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-09 13:31:51 +0400

    got info from html

--- parser.py
+++ parser.py
@@ -3,4 +3,10 @@ from bs4 import BeautifulSoup
 with open('index.html', mode='rb') as html:
     src = html.read()
 soup = BeautifulSoup(src, 'lxml')
-print(soup.find('tr').text)
+tr_list = soup.find(class_='simple-little-table trades-table').find_all('tr')
+for tr in tr_list:
+    # for td in tr.find_all('td'):
+    black_list_of_indexes = [1, 3, 4, 5, 6, 8, 9, 10, 19, 20, 21]
+    # black_list_of_indexes = []
+    print([elem for i, elem in enumerate(tr.text.strip().split('\n')) if i not in black_list_of_indexes ])
+    print('end')
