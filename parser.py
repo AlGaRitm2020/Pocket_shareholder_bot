944a773fc5e710d1dd0b61924d80c72493fa72e6
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-09 13:37:29 +0400

    optimized data

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
-
+from pprint import pprint
 with open('index.html', mode='rb') as html:
     src = html.read()
 soup = BeautifulSoup(src, 'lxml')
 tr_list = soup.find(class_='simple-little-table trades-table').find_all('tr')
-for tr in tr_list:
-    # for td in tr.find_all('td'):
-    black_list_of_indexes = [1, 3, 4, 5, 6, 8, 9, 10, 19, 20, 21]
-    # black_list_of_indexes = []
-    print([elem for i, elem in enumerate(tr.text.strip().split('\n')) if i not in black_list_of_indexes ])
+black_list_of_indexes = [1, 3, 4, 5, 6, 8, 9, 10, 19, 20, 21]
+data = []
+for i, tr in enumerate(tr_list):
+    if i > 1 and i < 260:
+        data.append([elem for i, elem in enumerate(tr.text.strip().split('\n')) if i not in black_list_of_indexes ])
     print('end')
+pprint(data)
