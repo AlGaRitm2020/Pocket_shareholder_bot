70ed57ce806101e5ab76fc6485e6454b2548fa23
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-09 15:08:04 +0400

    get response without save index.html

--- get_html.py
+++ get_html.py
@@ -1,9 +1,9 @@
 import requests
 
+
 def get_html(URL):
     response = requests.get(URL).content
-    with open('index.html', mode='wb') as html:
-        html.write(response)
+    return response
 
 
 get_html('https://smart-lab.ru/q/shares/')
