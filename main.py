9a01f9233bc63b02081b7e9a33492adc5002f479
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-10 07:29:17 +0400

    more actives after entered keyword

--- main.py
+++ main.py
@@ -25,9 +25,10 @@ def start(update: Update, context: CallbackContext):
     return 1
 
 
-def sort_by_profit(update: Update, context: CallbackContext, reverse=True, period='12 month'):
+def sort_by_profit(update: Update, context: CallbackContext, reverse=True, period='12 month', start_index=0):
     """
     Print most profit actives per 12 week/month/year/month
+    You can set reverse for sorting
     """
     if period == '12 month':
         sort_index = 16
@@ -43,35 +44,42 @@ def sort_by_profit(update: Update, context: CallbackContext, reverse=True, perio
         grow_period = 'недельный'
 
     most_profit_data = sorted(data, key=lambda x: x[sort_index], reverse=reverse)
-    for i in range(15):
+    for i in range(start_index, start_index + 15):
         (update.message.reply_text(
             f'{i + 1} "{most_profit_data[i][3]}" {grow_period} рост: {most_profit_data[i][sort_index]}'))
 
 
 def stream(update, context):
+
     """
     main handler for messages
     checking keywords in message
     """
 
     is_answered = False
+    content_count_per_page = 15
     stems = get_stems(update.message.text)
     print(stems)
-
-    reverse = True
+    global period_gl, start_index_gl, reverse_gl
+    reverse_gl = True
     if check_stems(stems, KeyWords.increase):
-        reverse = False
+        reverse_gl = False
     if check_stems(stems, KeyWords.profit):
+        start_index_gl = 0
         if check_stems(stems, KeyWords.week):
-            sort_by_profit(update, context, reverse=reverse, period='week')
+            period_gl = 'week'
         elif check_stems(stems, KeyWords.month):
-            sort_by_profit(update, context, reverse=reverse, period='month')
+            period_gl = 'month'
         elif check_stems(stems, KeyWords.year):
-            sort_by_profit(update, context, reverse=reverse, period='year')
-        elif check_stems(stems, KeyWords.month_12):
-            sort_by_profit(update, context, reverse=reverse, period='12 month')
+            period_gl = 'year'
         else:
-            sort_by_profit(update, context, reverse=reverse)
+            period_gl = '12 month'
+
+        sort_by_profit(update, context, reverse=reverse_gl, period=period_gl, start_index=start_index_gl)
+    elif check_stems(stems, KeyWords.extra_content):
+        start_index_gl += content_count_per_page
+        sort_by_profit(update, context, reverse=reverse_gl, period=period_gl,
+                       start_index=start_index_gl)
 
 
 def main() -> None:
@@ -80,7 +88,6 @@ def main() -> None:
 
     text_handler = MessageHandler(Filters.text, stream)
 
-    # Регистрируем обработчик в диспетчере.
     dispatcher.add_handler(text_handler)
 
     updater.start_polling()
