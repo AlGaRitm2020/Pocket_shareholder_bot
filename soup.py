f9c0c81a571f71a6d627b589bd89157dede1e054
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-10 07:48:08 +0400

    transformed data to dict type (key - company name)

--- soup.py
+++ soup.py
@@ -9,32 +9,27 @@ def get_data():
     src = get_html(URL)
     soup = BeautifulSoup(src, 'lxml')
     tr_list = soup.find(class_='simple-little-table trades-table').find_all('tr')
-    black_list_of_indexes = [1, 2, 4, 12, 11, 20, 21]
-    black_list_of_indexes = []
-    data = []
+    data = {}
     for i, tr in enumerate(tr_list):
         if 1 < i < 260:
-            data.append([elem for i, elem in enumerate(tr.text.split('\n')) ])
-            #if
-            #i not in black_list_of_indexes and elem and elem != '+' and '\t' not in elem]
-    for i, td in enumerate(data):
+            company_name = tr.text.split('\n')[3]
+            data[company_name] = [elem for i, elem in enumerate(tr.text.split('\n'))]
 
-        if len(td) == 28:
+    for i, company_name in enumerate(data):
+        if len(data[company_name]) == 28:
             for _ in range(4):
-                del data[i][5]
-        print(len(data[i]))
-        for j, elem in enumerate(td):
+                del data[company_name][5]
+        for j, elem in enumerate(data[company_name]):
             to_float_list = [8, 13, 14, 15, 16, 17, 18, 19, 20]
             if j in to_float_list:
                 if elem and '\t' not in elem:
                     try:
-
-                        data[i][j] = float(elem[:-1].replace(' ', ''))
+                        data[company_name][j] = float(elem[:-1].replace(' ', ''))
                     except:
 
-                        data[i][j] = int(elem)
+                        data[company_name][j] = int(elem)
                 else:
-                    data[i][j] = 0.001
+                    data[company_name][j] = 0.001
 
     return data
 
